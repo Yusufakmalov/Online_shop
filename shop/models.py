e185955bdbdf678967060e9e6f36d83d4bea27c9
salimov1535 <salimovabdullox8@gmail.com>
2025-05-13 12:20:03 +0500

    second commit

--- shop/models.py
+++ shop/models.py
@@ -1,37 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import User
+
+class Category(models.Model):
+        name = models.CharField(max_length=200)
+        slug = models.SlugField(max_length=200,
+                                unique=True)
+
+class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+ 
+ 
+def __str__(self):
+    return self.name
+
+
 
 
 class Product(models.Model):
+    category = models.ForeignKey(Category,
+                            related_name='products',
+                            on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to='products/%Y/%m/%d/')
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                            blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,
+                               decimal_places=2)
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True)
-    address = models.CharField(max_length=255, blank=True)
-
-class DeletedProduct(models.Model):
-    id = models.AutoField(primary_key=True)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    deleted_name = models.CharField(max_length=255)
-    deleted_at = models.DateTimeField(auto_now_add=True)
-
-
-class ProductDiscount(models.Model):
-    id = models.AutoField(primary_key=True)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    discount = models.DecimalField(max_digits=5, decimal_places=2) 
-    start_time = models.DateTimeField()  
-    end_time = models.DateTimeField()  
-
+ 
+ 
+ 
+    class Meta:
+       ordering = ['name']
+       indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+ ]
     def __str__(self):
-        return f"{self.discount}% discount on {self.product.name} from {self.start_time} to {self.end_time}"
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-def product_list(request):
-    products = Product.objects.filter(active=True)
-    return render(request, 'shop/product_list.html', {'products': products})
 
-def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    return render(request, 'shop/product_detail.html', {'product': product})
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category,
+                                    slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                     'shop/product/list.html',
+                    {'category': category,
+                     'categories': categories,
+                     'products': products})
 
 
